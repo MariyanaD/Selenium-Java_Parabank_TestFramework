96706c55ea85971fddd63f2763ddb467eceba3b3
Mariyana Shutova <mariyana.doichinova@gmail.com>
2025-01-29 18:17:32 +0200

    Add files via upload

---
+++ src/test/java/core/TestData.java
@@ -0,0 +1,24 @@
+package core;
+
+public class TestData {
+        private static String username;
+        private static String password;
+
+        public static String getUsername() {
+            return username;
+        }
+
+        public static void setUsername(String username) {
+            TestData.username = username;
+        }
+
+        public static String getPassword() {
+            return password;
+        }
+
+        public static void setPassword(String password) {
+            TestData.password = password;
+        }
+}
+
+
